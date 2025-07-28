340bd3a0d72546fae0bd7cd19ddcdd63c8e656e9
Krishan Mehta <krishanmehta052001@gmail.com>
2025-07-28 16:13:08 +0530

    footer

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 // footer.js
 document.addEventListener("DOMContentLoaded", function() {
-  fetch("E:\infosys web development\Hardeep Sir (Live Project)\e-commerece\footer.html")
+  fetch("footer.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById("footer-placeholder").innerHTML = data;
